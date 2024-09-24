6d1d45ee788784407c001217b442441f9aa77114
Feng Chen <fchen14@lsu.edu>
2024-09-24 16:38:34 -0500

    modified the image location

--- common_step.sh
+++ common_step.sh
@@ -5,9 +5,9 @@
 #SBATCH -t 01:00:00
 #SBATCH -o commonstep.out
 
-/usr/bin/time \
-singularity run -B /work,/project ../centos7 \
+export SIGRUN='singularity run -B /work,/project /project/wxx6941/packages/wu_sizegap.sif'
+/usr/bin/time $SIGRUN \
 searchKeys-11-5-18.py --sample_name t1 --path ./sample_root --search_mode 0 --use_common_keys
-/usr/bin/time \
-singularity run -B /work,/project ../centos7 \
+
+/usr/bin/time $SIGRUN \
 searchKeys_A_not_other.py --sample_name t1 --path ./sample_root --search_mode 6 --b_percent 0
