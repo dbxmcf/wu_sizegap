58f070d3804e2ab8fcc906212dc208980d7759ba
Feng Chen <fchen14@lsu.edu>
2024-09-24 16:19:42 -0500

    changed PBS to Slurm for commonstep

--- common_step.sh
+++ common_step.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-#PBS -q workq
-#PBS -l nodes=1:ppn=20
-#PBS -l walltime=00:30:00
-#PBS -j oe
-#PBS -N common_step
-#PBS -o common_step.out
+#SBATCH -p single
+#SBATCH -N 1
+#SBATCH -n 20
+#SBATCH -t 01:00:00
+#SBATCH -o commonstep.out
 
-module load python/2.7.12-anaconda-tensorflow
-
-cd $PBS_O_WORKDIR
-source activate /usr/local/packages/python/2.7.12-anaconda-wu/
-export PATH=/usr/local/packages/python/2.7.12-anaconda-wu/wuportal/Sample_Generation/:$PATH
-export PATH=/usr/local/packages/python/2.7.12-anaconda-wu/wuportal/Classification/lib:$PATH
-export PATH=/usr/local/packages/python/2.7.12-anaconda-wu/wuportal/Classification:$PATH
-export PATH=/usr/local/packages/python/2.7.12-anaconda-wu/wuportal/Motif_Discovery:$PATH
-
-/usr/bin/time searchKeys-11-5-18.py --sample_name t1 --path ./sample_root --search_mode 0 --use_common_keys
-/usr/bin/time searchKeys_A_not_other.py --sample_name t1 --path ./sample_root --search_mode 6 --b_percent 0
-
+/usr/bin/time \
+singularity run -B /work,/project ../centos7 \
+searchKeys-11-5-18.py --sample_name t1 --path ./sample_root --search_mode 0 --use_common_keys
+/usr/bin/time \
+singularity run -B /work,/project ../centos7 \
+searchKeys_A_not_other.py --sample_name t1 --path ./sample_root --search_mode 6 --b_percent 0
